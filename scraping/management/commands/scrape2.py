e87bdbbd38ea1a1918858416676d21d63d3c655d
arsens <aa@aa.lv>
2020-02-08 09:49:42 +0200

    new models added

--- scraping/management/commands/scrape2.py
+++ scraping/management/commands/scrape2.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.by import By
 class Command(BaseCommand):
     help = "collect jobs"
 
-    # define logic of command
     def handle(self, *args, **options):
         html = urlopen('https://www.cv.lv/darba-sludinajumi/q-automation')
         soup = BeautifulSoup(html, 'html.parser')
@@ -18,13 +17,23 @@ class Command(BaseCommand):
         for p in postings:
             url = p.find('a', itemprop='title')['href']
             title = p.find(itemprop='title').text
+
             alga = p.find(class_='salary-blue')
             if alga is not None:
                 alga_value = alga.get_text()
+
+            company = p.find(itemprop='hiringOrganization').text
+
+            location = p.find(itemprop='jobLocation')
+            if location is not None:
+                location_value = location.get_text()
+
             try:
                 Job.objects.create(
                     title=title,
                     alga=alga_value,
+                    company=company,
+                    location=location_value,
                     url=url
                 )
                 print('%s added' % (title,))
--- scraping/models.py
+++ scraping/models.py
@@ -6,6 +6,8 @@ class Job(models.Model):
     url = models.CharField(max_length=250, unique=True)
     title = models.CharField(max_length=250)
     alga = models.CharField(max_length=250)
+    company = models.CharField(max_length=250)
+    location = models.CharField(max_length=250)
     created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
